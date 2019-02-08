6a47e0fe9cb97ffb687f09e6ebe87a80232b45e2
Brian Mego <brianmego@gmail.com>
2019-02-07 21:45:15 -0600

    Problem 2

---
+++ c/002.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, c, total;
+    a = 1;
+    b = 1;
+    total = 0;
+
+    while (b < 4000000) {
+        c = b;
+        b = a + b;
+        a = c;
+        if (b % 2 == 0) {
+            total += b;
+        }
+    }
+    printf("%d\n", total);
+}
+
+
---
+++ rust/src/bin/002.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut a = 1;
+    let mut b = 1;
+    let mut total = 0;
+
+    while b < 4000000 {
+        let c = b;
+        b = a + b;
+        a = c;
+        if b % 2 == 0 {
+            total += b;
+        }
+    }
+    println!("{}", total)
+}
