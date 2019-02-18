7b6d18fc3cd4c5294ffbc43c8922ddc60381d4e5
Brian Mego <brianmego@gmail.com>
2019-02-17 22:05:04 -0600

    Problem 3

--- python/003.py
+++ python/003.py
@@ -1,11 +1,14 @@
-primes = []
-max_num = 600851475143
-for i in range(2, max_num):
-    prime = True
-    for j in primes:
-        if i % j == 0:
-            prime = False
-            break
-    if prime:
-        primes.append(i)
-        print(i)
+target_num = 600851475143
+test_val = 3
+prime_list = [2]
+while test_val < (target_num ** (1/2)):
+    is_prime = True
+    for i in prime_list:
+        if test_val % i == 0:
+            is_prime = False
+            test_val += 2
+    if is_prime:
+        prime_list.append(test_val)
+        if target_num % test_val == 0:
+            target_num = int(target_num / test_val)
+print(target_num)
