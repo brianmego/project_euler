b11367da86be95f55437c6e1139951a172f67ba8
Brian Mego <brianmego@gmail.com>
2019-04-15 22:03:45 -0500

    Solves problem 4 in python

---
+++ python/004.py
@@ -0,0 +1,15 @@
+def is_palindrome(i):
+    return str(i) == str(i)[::-1]
+
+def has_three_digit_factors(i):
+    for j in range(999, 500, -1):
+        if i % j == 0:
+            quotient = i / j
+            if quotient > 100 and quotient <= 999:
+                return True
+
+for i in range(999999, 10000, -1):
+    if is_palindrome(i):
+        if has_three_digit_factors(i):
+            print(i)
+            break
