4f99882e07f212edbbd403ee9a8a685b62e45c80
Brian Mego <brianmego@gmail.com>
2019-11-30 09:35:14 -0600

    Solves problem 5 in python

---
+++ python/005.py
@@ -0,0 +1,14 @@
+step_size = 20
+target = step_size
+numbers_to_attempt = range(target, 0, -1)
+
+while True:
+    success = True
+    for divisor in numbers_to_attempt:
+        if target % divisor != 0:
+            success = False
+            break
+    if success:
+        print(f'Answer: {target}')
+        break
+    target += step_size
