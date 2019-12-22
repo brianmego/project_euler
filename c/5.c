58d5e2e38804269a154f473b53892319997230e9
Brian Mego <brianmego@gmail.com>
2019-12-21 22:25:01 -0600

    Cleans up 5

--- c/5.c
+++ c/5.c
@@ -5,19 +5,12 @@ int main()
 {
     int step_size = 20;
     int target = step_size;
-    int numbers_to_attempt[target];
-    int counter, i;
-
-    counter = 0;
-    for (i=target; i > 0; i--) {
-        numbers_to_attempt[counter] = i;
-        counter++;
-    }
+    int i;
 
     while (1) {
         bool success=true;
-        for (i=0; i>=sizeof(numbers_to_attempt); i++){
-            if (target % numbers_to_attempt[i]) {
+        for (i=1; i<=step_size; i++){
+            if (target % i) {
                 success=false;
                 break;
             }
