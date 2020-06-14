0387f01f96e30494771987320f90aa73b3f44580
Brian Mego <brianmego@gmail.com>
2020-06-14 10:54:27 -0500

    Renames

--- erlang/prog4.erl
+++ erlang/prog4.erl
@@ -15,11 +15,11 @@ is_palindrome(X) -> X =:= lists:reverse(X).
 
 has_three_digit_factors(X) -> has_three_digit_factors(X, 999, 100).
 
-has_three_digit_factors(_, CurrentTest, Smallest) when CurrentTest == Smallest - 1 -> false;
-has_three_digit_factors(X, CurrentTest, Smallest) when X rem CurrentTest > 0 -> has_three_digit_factors(X, CurrentTest - 1, Smallest); 
-has_three_digit_factors(X, CurrentTest, Smallest) when X rem CurrentTest == 0 -> 
-    Quotient = X div CurrentTest,
+has_three_digit_factors(_, N1, N2) when N1 == N2 - 1 -> false;
+has_three_digit_factors(X, N1, N2) when X rem N1 > 0 -> has_three_digit_factors(X, N1 - 1, N2);
+has_three_digit_factors(X, N1, N2) when X rem N1 == 0 ->
+    Quotient = X div N1,
     if 
        Quotient >= 100 andalso Quotient =< 999 -> true;
-       Quotient < 100 orelse Quotient > 999 -> has_three_digit_factors(X, CurrentTest - 1, Smallest)
+       Quotient < 100 orelse Quotient > 999 -> has_three_digit_factors(X, N1 - 1, N2)
     end.
